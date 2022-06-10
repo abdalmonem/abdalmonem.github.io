a802733d160be6140d21cee37032d69ca50b2528
abdalmonem <abnhamoda@gmail.com>
2022-06-10 09:59:46 +0200

    fd

--- flutter.js
+++ flutter.js
@@ -33,7 +33,7 @@ _flutter.loader = null;
      */
     loadEntrypoint(options) {
       const {
-        entrypointUrl = "main.dart.js",
+        entrypointUrl = "main.dart.js.gz",
         serviceWorker,
       } = (options || {});
       return this._loadWithServiceWorker(entrypointUrl, serviceWorker);
